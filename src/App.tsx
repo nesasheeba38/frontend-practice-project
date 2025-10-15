6c52b70063489a91cf6eff865ea02b7c679fb0e5
nesasheeba38 <nesasheebaj32@gmail.com>
2025-10-15 22:47:44 +0530

    files are updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { HeaderNavbar } from './HeaderSection/HeaderNavBar'
 import {Projects} from './BodySection/Projects'
-// import './App.css'
+import './App.css'
 import { FooterContent } from './FooterSection/FooterContent'
 function App() {
 
